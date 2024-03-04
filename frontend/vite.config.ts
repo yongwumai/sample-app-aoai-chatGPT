b86ccc1ff6ddde8c96cba9e6827e073ad540b7ce
麦琳 <lyonmai@126.com>
2024-03-04 16:54:45 +0800

    change: 修改配置。

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     build: {
-        outDir: "../api/static",
+        outDir: "../static",
         emptyOutDir: true,
         sourcemap: true
     },
